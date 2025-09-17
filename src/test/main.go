3b69688c226a7b7a306c26e589f504d829f77996
Mikail <mikail.liazidgracia@ynov.com>
2025-09-17 13:57:32 +0200

    ADD rework merchant

--- src/test/main.go
+++ src/test/main.go
@@ -6,26 +6,20 @@ import (
 )
 
 func main() {
+	character.ShowIntro()
 	personnage := character.CharacterCreation()
-
 	fmt.Println("\n=== Infos initiales du personnage ===")
 	character.DisplayInfo(personnage)
-
 	fmt.Println("\n--- Accès à l'inventaire ---")
 	character.AccessInventaire(personnage)
-
 	fmt.Println("\n--- Passage chez le marchand ---")
 	character.MerchantMenu(&personnage)
-
 	fmt.Println("\n--- Inventaire après achats ---")
 	character.AccessInventaire(personnage)
-
 	fmt.Println("\n--- Utilisation d'un objet ---")
 	character.UseItem(&personnage, "Bandage")
-
 	fmt.Println("\n--- Heal via fonction Heal ---")
 	character.Heal(&personnage)
-
 	fmt.Println("\n--- Simulation mort ---")
 	personnage.PointDeVie = 0
 	if character.IsDead(&personnage) {
